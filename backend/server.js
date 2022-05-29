886175aa8643e129d9650b2f8d8b8ac52c477f23
unknown <rajpreet@mind-roots.com>
2022-05-29 23:16:16 +0530

    code

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import routes from './src/routes'
 import cors from 'cors'
-import { CONNECTION_URL, DEBUG_MODE } from "./src/config";
+import { APP_PORT, CONNECTION_URL } from "./src/config";
 import errorHandler from "./src/middleware/errorHandler";
 const app = express()
 
@@ -22,16 +22,11 @@ app.use(express.urlencoded({
   extended: true
 }));
 
-if((process.env.NODE_ENV = "production") || (DEBUG_MODE = false)){
-  app.use(express.static("frontend/build"))
-}
-
 //routes
 app.use('/api', routes)
 
 app.use(errorHandler)
 
-const PORT = process.env.PORT || 4000
-app.listen(PORT, ()=> {
-    console.log(`App is running on ${PORT}`)
+app.listen(APP_PORT, ()=> {
+    console.log(`App is running on ${APP_PORT}`)
 })
--- backend/src/config/index.js
+++ backend/src/config/index.js
@@ -2,6 +2,7 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 export const {
+    APP_PORT,
     CONNECTION_URL,
     JWT_SECRET,
     REFRESH_SECRET,
